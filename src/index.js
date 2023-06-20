4a258d59d95798be6188c36651d08618149d9072
Vsevolod <sewa-1997@mail.ru>
2023-06-20 18:58:43 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom";
+import App from "./components/App/App.jsx";
+
+const container = document.getElementById("root")
+
+const root = ReactDOM.createRoot(container);
+
+root.render(<App/>)
+
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ReactRefreshWebpackPlugin  = require('@pmmmwh/react-refresh-webpack-plugin');
+
+let mode = "development";
+let target = 'web';
+if(process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  }),
+  new MiniCssExtractPlugin({
+    filename: "[name].[contenthash].css"
+  })
+];
+
+if(process.env.SERVE) {
+  plugins.push(new ReactRefreshWebpackPlugin())
+}
+
+module.exports = {
+  mode,
+  plugins,
+  target,
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    assetModuleFilename: "assets/[hash][ext][query]",
+    clean: true
+  },
+  devServer: {
+    hot: true
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(html)$/, use: ["html-loader"] 
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i, // /\.(le|c)ss$/i если вы используете less
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === 'production' ? 'asset' : 'asset/resource', // В продакшен режиме
+        // изображения размером до 8кб будут инлайнится в код
+        // В режиме разработки все изображения будут помещаться в dist/assets
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/, // не обрабатываем файлы из node_modules
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true, // Использование кэша для избежания рекомпиляции
+            // при каждом запуске
+          },
+        },
+      },
+    ]
+  }
+}
