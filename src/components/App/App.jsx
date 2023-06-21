e3eaa739689ec5482a8c4450accdc0114be8b30c
Vsevolod <seven@acceptsib.ru>
2023-06-22 01:49:28 +0700

    add react query

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,18 @@
 import Layout from "../Layout";
+import { QueryClientProvider, QueryClient } from "react-query";
+// import { ReactQueryDevtools } from "react-query-devtools";
 import "./App.scss";
 
+const queryClient = new QueryClient({})
+
 export default function App() {
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Layout>
         <span>Hello World!!!</span>
       </Layout>
-    </>
+      {/* // <ReactQueryDevtools initialIsOpen={true}/> */}
+    </QueryClientProvider>
   )
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,18 @@
 import Header from "../Header";
+import { useQuery } from "react-query";
 
 export default function Layout({children}) {
+    const { isLoading, isError, data, error } = useQuery("weather", () => {
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=3270906cf868939a75714f7803a5cb2b`)
+        .then(res => res.json())
+        .then(json => json)
+    })
+
     return (
         <>
             <Header/>
+            {isLoading ? "Loading..." : <span>{data}</span>}
+            {error ? error : "not error"}
             <main>
                 {children}
             </main>
